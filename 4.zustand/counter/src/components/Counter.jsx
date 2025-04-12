db87f71abb52cd2910694d0034c3efd33f8b8707
gyk0814 <49557580+gyk0814@users.noreply.github.com>
2025-04-13 03:02:22 +0900

    add: set value

--- 4.zustand/counter/src/components/Counter.jsx
+++ 4.zustand/counter/src/components/Counter.jsx
@@ -2,19 +2,33 @@ import React from "react";
 import useCounterStore from "../stores/counterStore";
 
 const Counter = () => {
-  const { number, increment, decrement, value, setValue } = useCounterStore();
+  const { number, increment, decrement, value, setValue, reset } =
+    useCounterStore();
+  const submit = (e) => {
+    e.preventDefault();
+    setValue(Number(e.target[0].value));
+  };
 
   return (
     <div className="counter-box">
-      <h1 style={{ marginBottom: "50px" }}>Counter</h1>
+      <h1 className="gradient-text" style={{ marginBottom: "30px" }}>
+        COUNTER
+      </h1>
       <div className="count">
         <button onClick={decrement}>-{value}</button>
         <h1>{number}</h1>
         <button onClick={increment}>+{value}</button>
       </div>
-      <div className="unit">
-        <input type="number" id="value" />
-        <button>단위 변경하기</button>
+      <div className="setting">
+        <form onSubmit={submit}>
+          <input type="number" id="input" min="1" placeholder={value} />
+          <input className="btn" type="submit" value={"단위 변경하기"} />
+        </form>
+        <img
+          src="https://endlessicons.com/wp-content/uploads/2012/11/reset-icon.png"
+          alt="reset"
+          onClick={reset}
+        />
       </div>
     </div>
   );
