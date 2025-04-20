261597f5095952c27e7feb84ac16d7f963ced2aa
gyk0814 <49557580+gyk0814@users.noreply.github.com>
2025-04-20 10:12:40 +0900

    fix: mobile screen

--- 5.Netflix/netflix/src/layouts/AppLayout.jsx
+++ 5.Netflix/netflix/src/layouts/AppLayout.jsx
@@ -79,7 +79,7 @@ const AppLayout = () => {
           <Navbar.Collapse id="navbarScroll">
             <Nav
               className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
+              style={{ maxHeight: "150px" }}
               navbarScroll
             >
               {navList.map((item, index) => (
