b2f3f779ecd2dea35c54f801082e4fede9c6ddbe
gyk0814 <49557580+gyk0814@users.noreply.github.com>
2025-04-21 22:34:33 +0900

    fix: responsive

--- 5.Netflix/netflix/src/components/MovieCard/MovieCard.jsx
+++ 5.Netflix/netflix/src/components/MovieCard/MovieCard.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import "./MovieCard.style.css";
 import Button from "react-bootstrap/Button";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Badge from "react-bootstrap/Badge";
 
 const MovieCard = ({ movie }) => {
   const [like, setLike] = useState(false);
+  const [mobileOpenCard, setMobileOpenCard] = useState(false);
+  const [startTouch, setStartTouch] = useState(null);
+  const [isSwiping, setIsSwiping] = useState(false);
+  const cardRef = useRef(null);
+
+  const handleTouchStart = (e) => {
+    const touch = e.touches[0];
+    setStartTouch({ x: touch.clientX, y: touch.clientY });
+  };
+
+  const handleTouchMove = (e) => {
+    const touch = e.touches[0];
+    const dx = Math.abs(touch.clientX - startTouch?.x);
+    const dy = Math.abs(touch.clientY - startTouch?.y);
+
+    if (dx > 10 || dy > 10) {
+      setIsSwiping(true);
+    }
+  };
 
   const handleTitle = (title) => {
     if (title.length > 10) {
@@ -14,9 +33,32 @@ const MovieCard = ({ movie }) => {
       return <div className="title">{title}</div>;
     }
   };
+  const handleTouchEnd = () => {
+    if (!isSwiping) {
+      setMobileOpenCard(true); // 탭일 때만 활성화
+    }
+    setIsSwiping(false);
+  };
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (cardRef.current && !cardRef.current.contains(event.target)) {
+        setMobileOpenCard(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("touchstart", handleClickOutside); // 모바일 대응
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("touchstart", handleClickOutside);
+    };
+  }, []);
   return (
     <div
-      className="movie-card"
+      ref={cardRef}
+      className={`movie-card ${mobileOpenCard ? "movie-card-focus" : ""}`}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
       style={{
         backgroundImage: `url(https://www.themoviedb.org/t/p/w1066_and_h600_bestv2${movie.backdrop_path})`,
       }}
