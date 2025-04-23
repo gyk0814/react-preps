d3c8c2e9464dc79a0763f73078c58481b512679e
gyk0814 <49557580+gyk0814@users.noreply.github.com>
2025-04-23 10:46:35 +0900

    add: pagination

--- 5.Netflix/netflix/src/pages/Movies/MoviesPage.jsx
+++ 5.Netflix/netflix/src/pages/Movies/MoviesPage.jsx
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchMovieQuery } from "../../hooks/useSearchMovie";
 import { useSearchParams } from "react-router";
 import { Container, Row, Col } from "react-bootstrap";
 import MoviePosterCard from "../../components/MoviePosterCard/MoviePosterCard";
+import ReactPaginate from "react-paginate";
 import "./MoviesPage.style.css";
+
 const MoviesPage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [page, setPage] = useState(1);
 
   const query = searchParams.get("q");
   const { data, isLoading, error, iserror } = useSearchMovieQuery(query, page);
+
+  useEffect(() => {
+    setPage(1);
+  }, [query]);
 
   if (isLoading) {
     console.log(query, page);
@@ -18,6 +24,10 @@ const MoviesPage = () => {
   if (iserror) {
     return <Alert variant="danger">Error: {error.message}</Alert>;
   }
+  const handlePageClick = (page) => {
+    console.log(page);
+    setPage(page.selected + 1);
+  };
 
   console.log(data);
   return (
@@ -34,14 +44,14 @@ const MoviesPage = () => {
               </Col>
             </Row>
           </Col>
-          <Col xs={12}>
-            <Row className="px-2">
+          <Col xs={12} className="movie-col" style={{ paddingLeft: "4.5rem" }}>
+            <Row className="px-2 ">
               {data?.results.map((movie, index) => (
                 <Col
                   xs={6}
                   md={4}
                   lg={3}
-                  xxl={2}
+                  xxl="auto"
                   key={index}
                   className="card-container"
                 >
@@ -49,6 +59,28 @@ const MoviesPage = () => {
                 </Col>
               ))}
             </Row>
+            <ReactPaginate
+              nextLabel=">"
+              onPageChange={handlePageClick}
+              pageRangeDisplayed={3}
+              marginPagesDisplayed={1}
+              pageCount={data?.total_pages}
+              previousLabel="<"
+              pageClassName="page-item"
+              pageLinkClassName="page-link"
+              previousClassName="page-item"
+              previousLinkClassName="page-link"
+              nextClassName="page-item"
+              nextLinkClassName="page-link"
+              breakLabel="..."
+              breakClassName="page-item"
+              onPageActive="active"
+              breakLinkClassName="page-link"
+              containerClassName="pagination"
+              activeClassName="active"
+              renderOnZeroPageCount={null}
+              initialPage={page - 1}
+            />
           </Col>
         </Row>
       </Container>
